364c3f05fe2ab27b81f3e9d5e8ae280a3178676a
Bruno Feiertag Martini <bruno.martini.gh@astemo.com>
2025-10-28 16:01:10 -0400

    subindo

--- zero/settings.py
+++ zero/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*2osh_x43+(h_uedf_5u!q_xi5h9lek43glqcsjw$b)-)ws7a6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -120,6 +120,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
